63deb42863af71ddbd1fbde6739ae09665b70bcb
ivankonkin <hello@ivankonkin.ru>
2023-04-17 22:34:31 +0300

    homework 4: Установка IDE и компиляция первой программы

--- src/HomeWork.java
+++ src/HomeWork.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 
 public class HomeWork {
     public static void main(String[] args) {
-        System.out.println("init commit");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter you full name");
+        String fullName = sc.nextLine();
+        String encodedFullName = Base64.getEncoder().encodeToString(fullName.getBytes());
+        System.out.println("Program result: " + encodedFullName);
     }
 }
