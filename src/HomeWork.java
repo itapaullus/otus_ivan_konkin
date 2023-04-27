b2652916e6d4ec60d6d89c2829ed29bc0d7a4f42
paullus <44482073+itapaullus@users.noreply.github.com>
2023-04-27 22:59:43 +0300

    Lesson 6, testing system (#2)
    Lesson 6 homework, testing system

--- src/HomeWork.java
+++ src/HomeWork.java
@@ -1,9 +1,11 @@
 import lesson1.*;
 import lesson4.FirstProgram;
+import lesson6.TestSystem;
 
 public class HomeWork {
     public static void main(String[] args) {
 //        Lesson1.main(new String[]{"asd"});
-        FirstProgram.main(null);    // run lesson4
+//        FirstProgram.main(null);    // run lesson4
+        TestSystem.main(null);
     }
 }
