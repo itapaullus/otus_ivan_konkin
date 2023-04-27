8019c736cb50e1fed2e35f4426079e565d6a87b0
i.konkin <i.konkin@tinkoff.ru>
2023-04-27 22:47:46 +0300

    structure for test project

--- src/HomeWork.java
+++ src/HomeWork.java
@@ -1,13 +1,7 @@
-import java.util.Base64;
-import java.util.Scanner;
-
+import lesson1.*;
 
 public class HomeWork {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter you full name");
-        String fullName = sc.nextLine();
-        String encodedFullName = Base64.getEncoder().encodeToString(fullName.getBytes());
-        System.out.println("Program result: " + encodedFullName);
+        Lesson1.main(new String[]{"asd"});
     }
 }
