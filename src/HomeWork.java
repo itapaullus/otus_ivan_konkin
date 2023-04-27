1b8d3413d801f3c5a28ec14f4a26561e7f1bacc2
paullus <44482073+itapaullus@users.noreply.github.com>
2023-04-27 22:55:58 +0300

    Merge pull request #3
    * Lesson 4 fix for new structure

--- src/HomeWork.java
+++ src/HomeWork.java
@@ -1,7 +1,9 @@
 import lesson1.*;
+import lesson4.FirstProgram;
 
 public class HomeWork {
     public static void main(String[] args) {
-        Lesson1.main(new String[]{"asd"});
+//        Lesson1.main(new String[]{"asd"});
+        FirstProgram.main(null);    // run lesson4
     }
 }
