84c5a5900b781c8bb66c2bc903114e544927dd9b
ivankonkin <hello@ivankonkin.ru>
2023-05-19 00:08:09 +0300

    Lesson 13, review fixes

--- src/lesson13/App.java
+++ src/lesson13/App.java
@@ -3,26 +3,26 @@ package lesson13;
 import lesson13.model.Account;
 import lesson13.model.Client;
 
-import java.util.List;
+import java.util.Set;
 
 public class App {
     public static void main(String[] args) {
         Bank bank = new Bank();
         // client 1
-        Client client1 = new Client("Ivanov", 32);
-        bank.addClient(client1);
-        bank.addAccount(new Account(1), client1);
+        bank.addClient(new Client("Ivanov", 32));
+        bank.addAccount(new Account(1), new Client("Ivanov", 32));
         // client 2
-        Client client2 = new Client("Petrov", 34);
-        bank.addClient(client2);
-        bank.addAccount(new Account(2), client2);
-        bank.addAccount(new Account(3), client2);
-        bank.addAccount(new Account(4), client2);
+        bank.addClient(new Client("Petrov", 34));
+        bank.addAccount(new Account(2), new Client("Petrov", 34));
+        bank.addAccount(new Account(3), new Client("Petrov", 34));
+        bank.addAccount(new Account(4), new Client("Petrov", 34));
         // getAccounts
-        List<Account> accountList = bank.getAccounts(client2);
+        Set<Account> accountList = bank.getAccounts(new Client("Petrov", 34));
         System.out.println(accountList);
         // getClient
         Client client = bank.findClient(new Account(1));
+        System.out.println(client);
+        client = bank.findClientFaster(new Account(1));
         System.out.println(client);
     }
 }
--- src/lesson13/Bank.java
+++ src/lesson13/Bank.java
@@ -3,24 +3,23 @@ package lesson13;
 import lesson13.model.Account;
 import lesson13.model.Client;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Bank {
-    public final Map<Client, List<Account>> clientAccounts;
+    public final Map<Client, Set<Account>> clientAccounts;
+    public final Map<Account, Client> accToClientMap;
 
     public Bank() {
         clientAccounts = new HashMap<>();
+        accToClientMap = new HashMap<>();
     }
 
-    public List<Account> getAccounts(Client client) {
-        return clientAccounts.getOrDefault(client, new ArrayList<>());
+    public Set<Account> getAccounts(Client client) {
+        return clientAccounts.getOrDefault(client, new HashSet<>());
     }
 
-    public Client findClient(Account account) {
-        for (Map.Entry<Client, List<Account>> entry : clientAccounts.entrySet()) {
+    public Client findClient(Account account) {  // O(N) search
+        for (Map.Entry<Client, Set<Account>> entry : clientAccounts.entrySet()) {
             if (entry.getValue().contains(account)) {
                 return entry.getKey();
             }
@@ -28,13 +27,17 @@ public class Bank {
         return null;
     }
 
+    public Client findClientFaster(Account account) {   //search by Map
+        return accToClientMap.getOrDefault(account, null);
+    }
     public void addAccount(Account account, Client client) {
-        List<Account> accounts = clientAccounts.getOrDefault(client, new ArrayList<>());
+        Set<Account> accounts = clientAccounts.getOrDefault(client, new HashSet<>());
         accounts.add(account);
         clientAccounts.put(client, accounts);
+        accToClientMap.put(account, client);
     }
 
     public void addClient(Client client) {
-        clientAccounts.put(client, new ArrayList<>());
+        clientAccounts.put(client, new HashSet<>());
     }
 }
